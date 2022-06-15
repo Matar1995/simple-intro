29ebcc27150be6de47f187ab6bdc1a75a970d0e7
ahmed matar <matar1995geocode@gmail.com>
2022-06-15 21:46:30 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode:"jit",
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors:{
+        primary:"#1B2430"
+      }
+    },
+  },
+  plugins: [],
+}
